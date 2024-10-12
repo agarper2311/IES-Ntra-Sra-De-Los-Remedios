95dbee2141ae4ba6beb0195bb36e971112fe2bc1
Subzero-03 <agarper3211@gmail.com>
2024-10-13 00:03:46 +0200

    Programa hola concatena implementado en python

---
+++ Programacion/Tema 1/Pseint/Python/hola_concatena.py
@@ -0,0 +1,12 @@
+def hola_concatena():
+
+    print('¡¡Vamos a concatenar palabras !!')
+
+    palabra1 = str(input('Introduzca su primera palabra: '))
+    palabra2 = str(input('Introduzca su segunda palabra: '))
+
+    resultado = (palabra1 + ' ' +palabra2)
+
+    print('Resultado concatenado: ', resultado)
+
+hola_concatena()
