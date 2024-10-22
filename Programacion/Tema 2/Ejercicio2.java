cc88546add8fc72dce0323eb5cfa9eee3319a09f
Subzero-03 <agarper3211@gmail.com>
2024-10-22 09:39:39 +0200

    Subida ejercicio 2 realizado en el bloc de notas y compilado mediante el cmd con el comando javac

---
+++ Programacion/Tema 2/Ejercicio2.java
@@ -0,0 +1,23 @@
+public class Ejercicio2 {
+    public static void main (String[] args) {
+        boolean logico;
+        char caracter;
+        byte enteroByte;
+        short enteroShort;
+        int enteroInt;
+        long enteroLong;
+        float real;
+        double realLargo;
+
+        // Inicialización de variables
+        logico = true;  // Corrección aquí
+        caracter = 'a';
+        enteroByte = 10;
+        enteroShort = 1;
+        enteroInt = 2000;
+        enteroLong = 10333L;  // Mejor práctica -> Le agregamos una L para que lo tome como tipo Long
+        real = 2.56F;  // Mejor práctica -> Le agregamos una F para que lo tome como tipo Float
+
+        realLargo = 3.1415926535;
+    }
+}
