504dc1fd32fdf332dd96bcc2c992ef658379a164
Subzero-03 <agarper3211@gmail.com>
2024-10-22 10:18:24 +0200

    Actualización del archivo Ejercicio2

--- Programacion/Tema 2/Ejercicio2.java
+++ Programacion/Tema 2/Ejercicio2.java
@@ -9,15 +9,27 @@ public class Ejercicio2 {
         float real;
         double realLargo;
 
+	//Declaración Constantes
+	final int anyo = 2024;
+
         // Inicialización de variables
         logico = true;  // Corrección aquí
         caracter = 'a';
-        enteroByte = 10;
+        enteroByte = 1;
         enteroShort = 1;
         enteroInt = 2000;
         enteroLong = 10333L;  // Mejor práctica -> Le agregamos una L para que lo tome como tipo Long
         real = 2.56F;  // Mejor práctica -> Le agregamos una F para que lo tome como tipo Float
 
         realLargo = 3.1415926535;
+
+	//Desbordar una variable
+	//enteroByte = 235;
+	enteroByte = 127;
+	
+	// Mostrar una variable
+	System.out.println("El valor de la variable lógica es: "+ logico);
+
+	
     }
 }
