7620d4265d5ced5bef3373fad2674bdcf1cbac42
Subzero-03 <agarper3211@gmail.com>
2024-10-23 12:19:05 +0200

    Subida de archivos

---
+++ Programacion/Tema 2/MiPrimerProyecto/src/miprimerproyecto/MiPrimerProyecto.java
@@ -0,0 +1,18 @@
+package miprimerproyecto;
+
+/**
+ *
+ * @author agarc
+ */
+public class MiPrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+
+        
+    }
+    
+}
---
+++ Programacion/Tema 2/ValorAbsoluto/src/valorabsoluto/ValorAbsoluto.java
@@ -0,0 +1,18 @@
+package valorabsoluto;
+
+/**
+ *
+ * @author agarc
+ */
+public class ValorAbsoluto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = -3;
+        int valorAbsoluto = Math.abs(x);
+        System.out.println(valorAbsoluto);
+    }
+    
+}
