7fc66576232e9997dcdfabd3dd5bacaa5c708578
Subzero-03 <agarper3211@gmail.com>
2024-10-23 12:38:43 +0200

    Organización de proyectos en la carpeta ejercicios

---
+++ Programacion/Tema 2/Ejercicios/EntradaDeDatos/src/entradadedatos/EntradaDeDatos.java
@@ -0,0 +1,17 @@
+
+package entradadedatos;
+
+/**
+ *
+ * @author agarc
+ */
+public class EntradaDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
