fbc6a7edfbab2bbfe5854c63d1f7e6e3c6606395
Subzero-03 <agarper3211@gmail.com>
2024-11-11 11:01:00 +0100

    Subida de archivos

---
+++ Programacion/Tema 2/Ejercicios/Ejercicio27/src/ejercicio27/Ejercicio27.java
@@ -0,0 +1,20 @@
+package ejercicio27;
+
+/**
+ *
+ * @author agarc
+ */
+public class Ejercicio27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de 7 menores que 100:");
+
+         for (int i = 7; i < 100; i += 7) {
+             System.out.println(i);
+     }
+    
+    }
+}
